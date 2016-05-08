6285fd5f37387d91e4f1237f8964ca230773a792
Anil2601 <asdeo1@student.monash.edu>
2016-05-08 15:14:26 +1000

    Revert "Commit 2"
    This reverts commit 7beac4938fd552d20f10b68339f43f99c218dba3.

--- assignment2/code/js/addlocationPage.js
+++ assignment2/code/js/addlocationPage.js
@@ -1,10 +1,2 @@
 // Code for the Add Location page.
-var map;
-function initMap() 
-{
-    var myLatLng = {lat: -37.9120467, lng: 145.1343136};
-    map = new google.maps.Map(document.getElementById('map'), {
-    center: myLatLng,
-    zoom: 17
-    });
-};
+
