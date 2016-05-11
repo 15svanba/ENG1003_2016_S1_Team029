0efadde6cb2c6073f7eb290141f039ad6e1148b7
Anil2601 <asdeo1@student.monash.edu>
2016-05-11 13:35:53 +1000

    Geocoding on change
    Runs geocode function on change

--- assignment2/code/js/addlocationPage.js
+++ assignment2/code/js/addlocationPage.js
@@ -1,10 +1,32 @@
 // Code for the Add Location page.
 var map;
-function initMap() 
+function initMap()
 {
-    var myLatLng = {lat: 49.837982, lng: -18.808594};
-    map = new google.maps.Map(document.getElementById('map'), {
-    center: myLatLng,
-    zoom: 17
-    });
-};
+        map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 17,
+        center: {lat: -34.397, lng: 150.644}
+        });
+        var geocoder = new google.maps.Geocoder();
+
+        document.getElementById('addressInput').addEventListener('input', function() {
+          geocodeAddress(geocoder, map);
+        });
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+        var address = document.getElementById('addressInput').value;
+        geocoder.geocode({'address': address}, function(results, status) {
+          if (status === google.maps.GeocoderStatus.OK)
+          {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+              map: resultsMap,
+              position: results[0].geometry.location
+            });
+          } 
+            else
+          {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
+        });
+      }
