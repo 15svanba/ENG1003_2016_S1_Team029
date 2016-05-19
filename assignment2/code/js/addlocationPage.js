2c8a0f87fc4c1a33bd76b57307137312e5d5874b
Anil2601 <asdeo1@student.monash.edu>
2016-05-19 14:52:02 +1000

    Current Location Button
    Button runs Geolocation function

--- assignment2/code/js/addlocationPage.js
+++ assignment2/code/js/addlocationPage.js
@@ -8,7 +8,6 @@ function initMap()
         center: {lat: -34.397, lng: 150.644}
         });
         var geocoder = new google.maps.Geocoder();
-
         document.getElementById('addressInput').addEventListener('blur', function() {
           geocodeAddress(geocoder, map);
         });
@@ -29,6 +28,31 @@ function geocodeAddress(geocoder, resultsMap) {
           {
             alert('Geocode was not successful for the following reason: ' + status);
           }
-            //alert(JSON.stringify(results[0].geometry.location));
         });
       }
+function currentLocation(){
+    var infoWindow = new google.maps.InfoWindow({map: map});
+    if (navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+}
